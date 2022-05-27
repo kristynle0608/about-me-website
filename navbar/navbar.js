62b6aff0190e8d8bee37c2c0991badc64bececb8
Kristyn Le <kristynle0608@gmail.com>
2022-05-27 15:34:01 -0300

    Add stuff to panda

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -3,15 +3,3 @@ $(window).on('load', () => {
     $("#nav-placeholder").load("../navbar/navbar.html");
 
 });
-
-$(window).scroll(function () {
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-
-});
